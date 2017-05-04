0f393c7d91e21f1fd03f140655cdb0bc3f439a70
Nguyen Datio <DatioNguyen@outlook.com>
2017-05-04 20:03:40 +1000

    -Modify Pin input -Update new xamarin ---- Note: Delete package folder and clean project before build

--- ToiDau/ToiDau/Views/LoginPage2.xaml.cs
+++ ToiDau/ToiDau/Views/LoginPage2.xaml.cs
@@ -4,9 +4,46 @@ namespace ToiDau.Views
 {
     public partial class LoginPage2 : ContentPage
     {
+        int selectedStage = 0;
         public LoginPage2()
         {
             InitializeComponent();
+            entryPin1.Focus();
+        }
+
+
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string _text = ((Entry)sender).Text;      //Get Current Text
+            if (_text.Length > 1)       //If it is more than your character restriction
+            {
+                _text = _text.Remove(_text.Length - 1);  // Remove Last character
+                ((Entry)sender).Text = _text;        //Set the Old value
+            }
+
+            switch (selectedStage)
+            {
+                case 0:
+                    selectedStage++;
+                    entryPin2.Focus();
+                    break;
+                case 1:
+                    selectedStage++;
+                    entryPin3.Focus();
+                    break;
+                case 2:
+                    selectedStage++;
+                    entryPin4.Focus();
+                    break;
+                case 3:
+                    selectedStage++;
+                    buttonLogin.Focus();
+                    break;
+                default:
+                    buttonLogin.Focus();
+                    break;
+            }
         }
     }
 }
